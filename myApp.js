a55360b9d27077bccdf18a345d4d06fea2bf1107
Roman Tolika <romchikus1987@gmail.com>
2024-05-17 11:55:51 +0000

    added /json route

--- myApp.js
+++ myApp.js
@@ -3,18 +3,18 @@ let express = require('express');
 let app = express();
 let path = require('path');
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     console.log('__dirname is',__dirname)
     let absolutePath = path.join(__dirname, 'views', 'index.html');
     console.log('absolutePath',absolutePath)
     res.sendFile(absolutePath);
 });
 
+app.get('/json',(req,res)=>{
+    res.json({"message": "Hello json"})
+})
+
 app.use('/public', express.static(path.join(__dirname, 'public')));
-
-
-
-
 
 
 
